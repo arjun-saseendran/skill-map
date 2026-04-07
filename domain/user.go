f0e8b62c97c8b35291a741cbf5501eeba0d23b0a
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-07 13:45:51 +0530

    add user domain

--- domain/user.go
+++ domain/user.go
@@ -1,8 +1,9 @@
-package main
+package domain
 
 import (
 	"errors"
 
+	"github.com/arjun-saseendran/skill-map/db"
 	"github.com/arjun-saseendran/skill-map/dto"
 	"github.com/arjun-saseendran/skill-map/models"
 )
@@ -10,6 +11,9 @@ import (
 type User interface {
 	Create(userData dto.UserCreateInput) (*models.User, error)
 	List() ([]models.User, error)
+	Get(id string) (*models.User, error)
+	Update(id string, userData *dto.UserUpdateInput) (*models.User, error)
+	Delete(id string) error
 }
 type user struct{}
 
@@ -27,13 +31,36 @@ func (u *user) Create(userData dto.UserCreateInput) (*models.User, error) {
 }
 
 func (u *user) List() ([]models.User, error) {
-	users := []models.User{}
+	var users []models.User
 	db.DB.Find(&users)
 	return users, nil
 }
 
-func (u *user) Get(string id) (*models.User, error) {
+func (u *user) Get(id string) (*models.User, error) {
 	singleUser := models.NewUser()
-	db.DB
+	db.DB.First(&singleUser, id)
+	if singleUser.ID == 0 {
+		return nil, errors.New("no user found")
+	}
+	return singleUser, nil
+}
 
+func (u *user) Update(id string, userData *dto.UserUpdateInput) (*models.User, error) {
+	updateUser := models.NewUser()
+	db.DB.First(updateUser, id)
+	if updateUser.ID == 0 {
+		return nil, errors.New("user not found")
+	}
+	db.DB.Model(&updateUser).Updates(models.User{FullName: userData.FullName, Email: userData.Email})
+	return updateUser, nil
+}
+
+func (u *user) Delete(id string) error {
+	deleteUser := models.NewUser()
+	db.DB.First(deleteUser, id)
+	if deleteUser.ID == 0 {
+		return errors.New("user not found")
+	}
+	db.DB.Delete(deleteUser)
+	return nil
 }
