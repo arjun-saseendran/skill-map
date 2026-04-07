35eb79714d543367dad507f2d9baf6f75ebcff04
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-07 09:18:41 +0530

    add user model

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint           `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"_"`
+	FullName  string         `json:"full_name"`
+	Email     string         `json:"email"`
+}
+
+func NewUser() *User {
+	return &User{}
+}
